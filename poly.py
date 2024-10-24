c8d7596b9a22999bcbcd64a5f77c23d5823de96e
NiccoF <111521406+NiccoF@users.noreply.github.com>
2024-10-24 16:07:32 -0500

    Try switching the order of the mult and add in main()

--- poly.py
+++ poly.py
@@ -211,9 +211,10 @@ def main():
         coeff_and_exp = next_line.split(" ")
         p.insert_term(int(coeff_and_exp[0]), int(coeff_and_exp[1]))
     #sum_lists = p.add(q)
+    print(p.mult(q))
     print(p.add(q))
     #product = p.mult(q)
-    print(p.mult(q))
+    
 
 
 
